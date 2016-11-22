79656bffff636da7d0c31a983d62951b2d4a3563
Yunkun <lyvius2@naver.com>
2016-11-22 16:41:49 +0900

    책 가격비교표 Template 80%

--- www/js/app.js
+++ www/js/app.js
@@ -20,7 +20,7 @@ var app = angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-}).controller('starterCtrl', function($scope, $http, $q, $ionicModal, $timeout, $ionicLoading, $ionicScrollDelegate, $ionicPopup, $ionicActionSheet){
+}).controller('starterCtrl', function($scope, $http, $q, $ionicModal, $timeout, $ionicLoading, $ionicScrollDelegate, $ionicPopup, $ionicActionSheet, $ionicPopover){
   var parent = this;
   var start = 1;
 
@@ -35,9 +35,9 @@ var app = angular.module('starter', ['ionic'])
     });
   };
 
-  var loading = function(callback) {
+  var loading = function(word, callback) {
     $ionicLoading.show({
-      template: '<i class="fa fa-refresh fa-spin fa-fw light"></i> Loading...'
+      template: '<i class="fa fa-refresh fa-spin fa-fw light"></i> ' + word + '...'
     }).then(function(){
       return callback();
     })
@@ -58,9 +58,32 @@ var app = angular.module('starter', ['ionic'])
     if(error) {
       console.log(error);
     } else {
+      $ionicLoading.hide();
       if(parent.findBookList == null) {
         parent.findBookList = data;
-        parent.openResultModal();
+        if(typeof parent.findBookList.rss.channel.item.length == 'undefined') {
+          var item = parent.findBookList.rss.channel.item;
+          var isbn = item.isbn13;
+          if(!isbn) isbn = item.isbn10;
+          loading('Checking price', function(){
+            inquiryPrice(isbn, function(error, data){
+              $ionicLoading.hide();
+              if(error) {
+                console.log(error);
+              } else {
+                item.aladin = data[0];
+                item.yes24 = data[1];
+                item.interpark = data[2];
+                item.bandi = data[3];
+                item.status = 'bestPrice';
+                parent.sellBookList.push(item);
+              }
+            });
+          });
+          console.log(parent.findBookList.rss.channel.item.isbn13);
+        } else {
+          parent.openResultModal();
+        }
       } else if(parent.findBookList.rss) {
         parent.findBookList.rss.channel.item = parent.findBookList.rss.channel.item.concat(data.rss.channel.item);
       }
@@ -71,35 +94,50 @@ var app = angular.module('starter', ['ionic'])
           parent.closeResultModal();
         });
       }
-      $ionicLoading.hide();
       angular.element('#findBookLayer').on('scroll', scrollHandler);
     }
     return;
   };
 
-  var inquiryPriceHandler = function(isbn, callback) {
+  var inquiryPrice = function(isbn, callback) {
     var getSellPrice = function(flatform){
       return $http.jsonp('https://usedbookserver.herokuapp.com/sellPrice?callback=JSON_CALLBACK', {params:{flatform:flatform,keyword:isbn}});
+    };
+
+    var getSellPriceBandi = function(){
+      return $http.jsonp('http://222.122.120.242:7570/ksf/api/search?callback=JSON_CALLBACK', {params:{sn:'usedbook',q:isbn,s:'bpr'}});
+    };
+
+    var priceHandler = function(prev, next) {
+      var item, value = null;
+      console.log('next.data', next.data);
+      if(typeof next.data.data == 'object' && next.data.data != null && next.data.data.length > 0){   // 3개사
+        item = next.data.data[0];
+        value = {
+          available:item.available,
+          bestPrice:item.bestPrice,
+          normalPrice:item.normalPrice,
+          lowPrice:item.lowPrice
+        };
+      } else if(typeof next.data.result == 'object' && typeof next.data.result.length != 'undefined'){  // 반디앤루니스
+        item = next.data.result[0];
+        value = {
+          available:(item.buy_rate != '' && item.buy_rate != '0')?true:false,
+          lowPrice:Number(item.buy_price)
+        };
+      }
+      prev.push(value);
+      return prev;
     };
 
     var serviceList = new Array();
     ['al','y2','ip'].forEach(function(item){
       serviceList.push(getSellPrice(item));
     });
+    serviceList.push(getSellPriceBandi());
+
     $q.all(serviceList).then(function(results){
-      results = results.reduce(function(prev, next) {
-        var value = null;
-        if(typeof next.data.data == 'object' && next.data.data.length > 0){
-          value = {
-            available:next.data.data[0].available,
-            bestPrice:next.data.data[0].bestPrice,
-            normalPrice:next.data.data[0].normalPrice,
-            lowPrice:next.data.data[0].lowPrice
-          };
-        }
-        prev.push(value);
-        return prev;
-      }, []);
+      results = results.reduce(priceHandler, []);
       return callback(null, results);
     }, function(error){
       return callback(error);
@@ -110,12 +148,12 @@ var app = angular.module('starter', ['ionic'])
   this.sellBookList = new Array();
 
   this.find = function(searchText, callback) {
-    loading(function(){
-      console.log('start', start);
+    loading('Searching for a book', function(){
       var uri = 'https://usedbookserver.herokuapp.com/api?callback=JSON_CALLBACK';
       //$http.jsonp('http://localhost:3000/api?callback=JSON_CALLBACK', {params:{keyword:searchText,start:start}})
       $http.jsonp(uri, {params:{keyword:searchText,start:start}})
         .then(function(result){
+          console.log(result.data);
           return callback(null, result.data);
         }, function(error){
           $ionicLoading.hide();
@@ -141,15 +179,16 @@ var app = angular.module('starter', ['ionic'])
       titleText: '매입 목록에 추가',
       destructiveText: '취소',
       buttonClicked: function(){
-        loading(function(){
-          inquiryPriceHandler(isbn, function(error, data){
+        loading('Checking price', function(){
+          inquiryPrice(isbn, function(error, data){
             $ionicLoading.hide();
             if(error) {
               console.log(error);
             } else {
               obj.aladin = data[0];
               obj.yes24 = data[1];
-              obj.interpatk = data[2];
+              obj.interpark = data[2];
+              obj.bandi = data[3];
               obj.status = 'bestPrice';
               parent.sellBookList.push(obj);
               console.log('sellBookList', parent.sellBookList);
@@ -169,6 +208,20 @@ var app = angular.module('starter', ['ionic'])
     }, 5000);
   };
 
+  this.getPriceByStatus = function(obj, status) {
+    return obj[status];
+  };
+
+  this.changeStatus = function(event, target, status, dom) {
+    event.preventDefault();
+    console.log('event', event);
+    console.log('dom', dom);
+    target.status = status;
+    $timeout(function(){
+      $scope.$apply();
+    }, 0);
+  };
+
   $ionicModal.fromTemplateUrl('findBookResult.html', {
     scope: $scope,
     animation:'slide-in-up'
@@ -186,6 +239,8 @@ var app = angular.module('starter', ['ionic'])
 
   angular.element(document.forms[0]).on('submit', function(e){
     e.preventDefault();
+    document.activeElement.blur();
+    console.log('isISBN?', validator.isISBN(parent.searchText));
     parent.find(parent.searchText, findSuccessHandler);
   });
 });
